824ae27eea61c0846269e7c5ac7f0968e44737f8
marcusSystementor <144769052+marcusSystementor@users.noreply.github.com>
2024-02-26 11:54:46 +0100

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,21 @@
+num1 = 5
+num2 = 5
+
+if num1 == num2:
+  print("Lika")
+elif num1 < num2:
+  print(num1, "är mindre än", num2)
+else:
+  print("Inget matchar")    
+
+
+num3 = 10
+num4 = 14
+
+userName = int(input("Enter your username "))
+password = int(input("Enter your password "))
+
+if userName == 5 and password == 6:
+  print("Welcome")
+else:
+  print("Please try again")  
---
+++ variabel.py
@@ -0,0 +1,10 @@
+my_name = "Marcus"
+nummer1 = 10
+nummer2 = 2
+number1 = 1 # Heltal, Integer, int
+number2 = 2.5 #Float, decimaltal
+myText = "Hello" #String, Sträng
+todayismonday = False #Boolean, true eller false, sant eller falskt
+my_list = [1, 4, 5, 6, 2, 1, 5, 6,7, 8] # list
+
+
